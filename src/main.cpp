eea8f608896410ee4752556e02c61d98ea953288
VillanuevaRFB <rodrigo.villanuevaa@usil.pe>
2025-04-10 21:28:46 -0500

    avanza y retrocede

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,47 @@
-#include <Arduino.h>
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Configurar entradas y salidas
+void config(void){
+    DDRD|=0xF0;//PD4 a PD7 como salidas
+    DDRB&=~0x70;//PB4 a PB6 como entrada
+    PORTB|=0x70;//pull-up
+}
+//Cambiar dirección del motor
+void direccion(char sentido){
+    switch(sentido){
+    case 0:
+        PORTD&=~0xF0;//apagar motores
+        break;
+    case 1:
+        PORTD&=~0xF0;//apagar motores
+        PORTD|=0x50;//PD4 y PD6 en alto
+        break;
+    case 2:
+        PORTD&=~0xF0;//apagar motores
+        PORTD|=0xA0;//PD5 y PD7 en alto
+        break;
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+int main(void){
+    config();
+    char sentido=0;//1=adelante, 2=reversa, 0=stop
+    char previo=1;//estado previo
+    direccion(sentido);//establecer dirección inicial
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    while(1){
+        if(!(PINB&0x10)&&previo){
+            _delay_ms(50);
+            if(!(PINB&0x10)){
+                sentido++;
+                if(sentido>2)
+                    sentido=0;
+                direccion(sentido);
+                while (!(PINB&0x10));
+                _delay_ms(50);
+            }
+        } 
+    }
 }
